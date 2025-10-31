614cbe8ed6361b840c78687a4dae05cfb2208774
David Shapiro <david@DavidsMacStudio.dhcp.lbl.gov>
2025-10-31 16:29:56 -0700

    Updates to the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,19 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os, sys
-sys.path.insert(0, os.path.abspath('../pystxmcontrol'))
-
 project = 'pystxmcontrol'
-copyright = '2022, David Shapiro (LBNL)'
-author = 'David Shapiro (LBNL)'
-release = '0.1'
+copyright = '2025, David Shapiro'
+author = 'David Shapiro'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','myst_parser']
 
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','myst_parser']
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -26,6 +23,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
