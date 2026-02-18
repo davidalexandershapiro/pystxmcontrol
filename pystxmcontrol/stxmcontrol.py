c9afe65c785101ec639aa5a2f9df07b1490ea2fc
aditter <asditter@gmail.com>
2026-02-18 11:15:29 -0800

    new branch for having xrf data collected only a few times during a scan

--- pystxmcontrol/stxmcontrol.py
+++ pystxmcontrol/stxmcontrol.py
@@ -3,13 +3,15 @@ from pystxmcontrol.gui.mainwindow import *
 import qdarktheme
 
 if __name__ == '__main__':
-    @atexit.register
-    def disconnectClient():
-        global widget
-        widget.disconnect()
 
     app = QtWidgets.QApplication([])
     app.setStyleSheet(qdarktheme.load_stylesheet("light"))
     widget = sampleScanWindow()
     widget.show()
+
+    @atexit.register
+    def disconnectClient():
+        global widget
+        widget.disconnect()
+
     sys.exit(app.exec())
