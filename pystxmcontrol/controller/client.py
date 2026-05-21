339efe6340bdb290947e84d4d46cea6a3679ec4a
David Shapiro <dashapiro@lbl.gov>
2026-05-21 14:40:52 -0700

    changed "host" to "stxm_address" in client and server

--- pystxmcontrol/controller/client.py
+++ pystxmcontrol/controller/client.py
@@ -172,7 +172,7 @@ class stxm_client(QtCore.QThread):
         self.listen = False
         self.lock = threading.Lock()
         self.client_config = json.loads(open(MAINCONFIGFILE).read())
-        self.server_address = self.client_config["server"]["host"]
+        self.server_address = self.client_config["server"]["stxm_address"]
         self.command_port = self.client_config["server"]["command_port"]
         self.data_port = self.client_config["server"]["stxm_data_port"]
         self.monitor_threads = []
